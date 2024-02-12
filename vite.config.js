7c46162f71aaeb2a0065ce0da48c7d251fa1c092
Artur Patura <arturpatura@gmail.com>
2024-02-12 08:42:23 +0100

    separete dev prod version

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,33 @@
-import { fileURLToPath, URL } from 'node:url';
-
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import fs from 'fs';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.scss', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    includeAbsolute: false,
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/css/app.scss', 'resources/js/app.js'],
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        includeAbsolute: false,
+                    },
                 },
+            }),
+        ],
+        server: {
+            host: true,
+            https: {
+                key: fs.readFileSync(env.VITE_SERVER_HTTPS_KEY),
+                cert: fs.readFileSync(env.VITE_SERVER_HTTPS_CERT),
             },
-        }),
-    ],
-    server: {
-        host: true,
-        https: {
-            key: fs.readFileSync("../cert/privkey1.pem"),
-            cert: fs.readFileSync("../cert/cert1.pem"),
+            hmr: {
+                host: env.VITE_SERVER_HMR_HOST,
+            },
         },
-        hmr: {
-            host: 'n86.pl',
-        },
-    },
+    };
 });
